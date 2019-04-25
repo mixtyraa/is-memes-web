7d3ddf93f9d3b21d2ab31c11dec682575f215de5
theporocenok <porocenok@bk.ru>
2019-04-26 03:49:12 +0500

    Add load tag by key and new type's field

--- script.js
+++ script.js
@@ -8,34 +8,7 @@ $(document).ready(function(){
 		success: function(msg){
 			if (msg["status"]=="success"){
 				msg["data"].forEach(function(item,msg){
-					$("#inputTypes").append(`<option id="${item["id"]}">${item["name"]}</option>`);
-				});
-			}
-		}
-	});
-
-	//Получение tags
-	$.get({
-		url: "http://localhost:3000/tags/",
-		dataType: "json",
-		success: function(msg){
-			if (msg["status"]=="success"){
-				msg["data"].forEach(function(item,msg){
-					$("#ListOfTags").append(`<li class="tag_item" id="${item["id"]}">${item["name"]}</li>`);
-					
-				});
-				$('.tag_item').on("click",function(){
-					var el = `<li class="choosen_tag_item show" id="${$(this).attr('id')}">${$(this).text()}<i class="material-icons clear-icon">clear</i></li>`;
-					console.log(el);
-					$("#ListOfChoosenTags").append(el);
-
-					$('.clear-icon').on("click",function(){
-						console.log($(this));
-						$('#ListOfTags').children('#'+$(this).parent().attr('id')).show();
-						$(this).parent().remove();
-					});
-
-					$(this).hide();
+					$("#ListOfTypes").append(`<li class="type_item" id="${item["id"]}">${item["name"]}</li>`);
 				});
 			}
 		}
@@ -46,7 +19,43 @@ $(document).ready(function(){
 		$("#ListOfTags").dropdown('show');
 		$('#ListOfTags li').prop('hidden',false);
 		$(`#ListOfTags li:not(:contains("${$("#inputTag").val()}"))`).prop('hidden', true);//Для показа только подходящих элементов
-		//$('#ListOfTags li:contains("01")').prop('hidden', true);//Для сокрытия элемента при выборе
+		
+		if ($(this).val()=="") {
+			$("#ListOfTags li").remove();
+		}
+
+		if ($(this).val()!="") {
+			$("#ListOfTags li").remove();
+			//Получение tags по введённой фразе
+			$.get({
+				url: "http://localhost:3000/tags/",
+				data: {q: $(this).val()},
+				success: function(msg){
+					if (msg["status"]=="success") {
+						msg["data"].forEach(function(item,msg){
+							if ($("#ListOfChoosenTags").find("#"+item["id"]).length==0) {
+								$("#ListOfTags").append(`<li class="tag_item" id="${item["id"]}">${item["name"]}</li>`);
+							}
+								
+						});
+						$('.tag_item').on("click",function(){
+							var el = `<li class="choosen_tag_item show" id="${$(this).attr('id')}">${$(this).text()}<i class="material-icons clear-icon">clear</i></li>`;
+							//console.log(el);
+							$("#ListOfChoosenTags").append(el);
+		
+							$('.clear-icon').on("click",function(){
+								console.log($(this));
+								$('#ListOfTags').children('#'+$(this).parent().attr('id')).show();
+								$(this).parent().remove();
+							});
+		
+							$(this).hide();
+						});
+					}
+				}
+
+			});
+		}
 	});
 
 	$('.choosen_tag_item').click(function(){
@@ -54,15 +63,5 @@ $(document).ready(function(){
 		$('#ListOfTags').children('#'+$(this).attr('id')).show();
 		$(this).removeClass('show');
 	});
-
-	/*$('.tag_item').on("click",function(){
-	//$('.tag_item').click(function(){
-		//$('#ListOfChoosenTags').children('#'+$(this).attr('id')).addClass('show');//append('<li class="choosen_tag_item" id="'+$(this).attr("id")+'">'+$(this).text()+'</li>');
-		//var el = `<li class="choosen_tag_item" id="${$(this).attr('id')}">${$(this).text()}<i class="material-icons clear-icon">clear</i></li>`;
-		//console.log(el);
-		//$("#ListOfChoosenTags").append(el);
-		console.log('check');
-		$(this).hide();
-	});*/
 
 })
